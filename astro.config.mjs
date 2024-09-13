f26c726882a67301323510b4dcb8bdcb112026f6
emp-temp <emptemp0000@gmail.com>
2024-09-14 01:50:39 +0900

    ci: 🎡 deploy周りの設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://emp-temp/github.io/profile-card',
+  site: 'http://emp-temp/github.io',
+  base: 'larao-profile-card'
 });
